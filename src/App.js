30be2f33c37a310ecece62e00375e3408c1fa6f6
David Delgado <david.delgadop88@gmail.com>
2023-09-04 17:03:40 -0400

    Using a single prop object and prop destructuring

--- src/App.js
+++ src/App.js
@@ -12,24 +12,16 @@ function App() {
     <div>
       <h1>Expense Tracker</h1>
       <ExpenseItem 
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        expense={expenses[0]}
       />
       <ExpenseItem 
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
+        expense={expenses[1]}
       />
       <ExpenseItem 
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
+        expense={expenses[2]}
       />
       <ExpenseItem 
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
+        expense={expenses[3]}
       />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
+const ExpenseItem = ({expense}) => {
     return (
         <div className='expense-item'>
-            <div>{props.date.toLocaleDateString()}</div>
+            <div>{expense.date.toLocaleDateString()}</div>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
+                <h2>{expense.title}</h2>
+                <div className='expense-item__price'>${expense.amount}</div>
             </div>
         </div>
     )
