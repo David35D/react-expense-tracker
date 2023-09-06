e03d1e1df1f719007855e06bdf4e3db9ceded601
David Delgado <david.delgadop88@gmail.com>
2023-09-06 12:26:17 -0400

    Manipulating states and handling events, 2 different approaches for each

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,27 @@
+// This component captures the data input by the user so that it will be used for generating an expense item.
+// There's an alternative code for managing state below the function.
+
 import React, { useState } from "react";
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    // Shared state with all the data in the form of an object.
     const [userInput, setUserInput] = useState({
         enteredTitle: '',
         enteredAmount: '',
         enteredDate: ''
     });
 
-    const titleChangeHandler = (event) => {
+    // Shared change handler to update the state.
+    const inputChangeHandler = (identifier, value) => {
         setUserInput((prevState) => {
             return {
                 ...prevState,
-                enteredTitle: event.target.value
+                [identifier]: value
             };
-        });
+        })
+        console.log(userInput);
     };
 
     return (
@@ -23,15 +29,15 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" onChange={(event) => inputChangeHandler('title', event.target.value)} /> {/* Special syntax to allow more than one paramater */}
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input type="number" min="0.01" step="0.01" onChange={(event) => inputChangeHandler('amount', event.target.value)} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2023-12-31"/>
+                    <input type="date" min="2019-01-01" max="2023-12-31" onChange={(event) => inputChangeHandler('date', event.target.value)} />
                 </div>
             </div>
             <div className="new-expense__actions">
@@ -42,3 +48,40 @@ const ExpenseForm = () => {
 };
 
 export default ExpenseForm;
+
+// In this alternative way of managing state we make an state for each input
+// There're also different versions for handling changes on the input
+/*
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+*/
