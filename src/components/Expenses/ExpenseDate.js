0308359b15856f68ea2300c4ffb9d31df3b91356
David Delgado <david.delgadop88@gmail.com>
2023-09-05 23:41:33 -0400

    Starting to work with state and forms

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,7 @@
 import './ExpenseDate.css';
 
 const ExpenseDate = ({date}) => {
+    // Parsing the date object into the desired format
     const month = date.toLocaleString('en-US', {month: 'long'});
     const day = date.toLocaleString('en-US', {day: '2-digit'});
     const year = date.getFullYear();
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import './Card.css';
 
 const Card = (props) => {
-    const classes = `card ${props.className}`;
+    const classes = `card ${props.className}`;      // Adding classes to the card dinamically
 
     return (
         <div className={classes}>{props.children}</div>
