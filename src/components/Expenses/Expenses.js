2a6ca5a8a494d13d1fc9bee77b81ff4acdd65d8a
David Delgado <david.delgadop88@gmail.com>
2023-09-07 09:15:04 -0400

    Added a temporal example for derived/computed states

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,6 +8,19 @@ import ExpensesFilter from './ExpensesFilter';
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2023');
 
+    let filterInfoText = '2019, 2020, 2021 & 2022'; // We use this instead of a new state because it depends from the state we already declared.
+    // In the end this whole function will re-execute everytime the year changes and so the whole conditional block will run again.
+
+    if (filteredYear === '2019') {
+        filterInfoText = '2020, 2021, 2022 & 2023';
+    } else if (filteredYear === '2020') {
+        filterInfoText = '2019, 2021, 2022 & 2023';
+    } else if (filteredYear === '2021') {
+        filterInfoText = '2019, 2020, 2022 & 2023';
+    } else {
+        filterInfoText = '2019, 2020, 2021 & 2023';
+    }
+
     const changeFilterHandler = (selectedYear) => {
         console.log('Selected year: ' + selectedYear);
         setFilteredYear(selectedYear);
@@ -19,6 +32,7 @@ const Expenses = (props) => {
                 onChangeFilter={changeFilterHandler}
                 selected={filteredYear} // Setting up 2-way binding, makes default value display when app reloads
             />
+            <p>{filterInfoText}</p> {/* Temporal for working with derived values */}
             <ExpenseItem 
                 expense={props.items[0]}
             />
